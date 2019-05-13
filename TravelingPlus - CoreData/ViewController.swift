2709ecd4cf14c1269c52d200d812b8f4a0227c85
Zewu Chen <contact.zewu@gmail.com>
2019-05-13 11:17:57 -0300

    Initial Commit

---
+++ TravelingPlus - CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelingPlus - CoreData
+//
+//  Created by Zewu Chen on 13/05/19.
+//  Copyright © 2019 Zewu Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
